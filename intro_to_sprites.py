909b3be0f70c3fb4c07794b73cddca069fafc99a
hyunsang4072 <hyunsang4072@gmail.com>
2023-05-31 22:22:38 -0700

    learn pygame

--- intro_to_sprites.py
+++ intro_to_sprites.py
@@ -1,12 +1,15 @@
 import pygame, sys
 
 class Hairyhair(pygame.sprite.Sprite):
-    def __init__(self, width, height, pos_x, pos_y, color):
+    def __init__(self, pic_path):
         super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+        self.image = pygame.image.load(pic_path)
+        # self.image.fill(color)
         self.rect = self.image.get_rect()
-        self.rect.center = [pos_x, pos_y]
+        # self.rect.center = [pos_x, pos_y]
+    
+    def update(self):
+        self.rect.center = pygame.mouse.get_pos()
 
 # General setup
 pygame.init()
@@ -16,11 +19,17 @@ clock = pygame.time.Clock()
 white = (255, 255, 255)
 
 # Game screen
-screen_width = 1920
-screen_height = 1080
+screen_width = 1920 // 2
+screen_height = 1080 // 2
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-hairyhair = Hairyhair(50, 50, 100, 100, white)
+# Set background
+background = pygame.image.load("Background_Blue.png")
+# No mouse showing on gamescreen
+pygame.mouse.set_visible(False)
+
+# Creating an instance
+hairyhair = Hairyhair("slice56_56.png")
 
 hairyhair_group = pygame.sprite.Group()
 hairyhair_group.add(hairyhair)
@@ -33,5 +42,9 @@ while True:
     
     
     pygame.display.flip()
+    screen.blit(background, (0, 0))
     hairyhair_group.draw(screen)
+    
+    # Get mouse position and update
+    hairyhair_group.update()
     clock.tick(60)
