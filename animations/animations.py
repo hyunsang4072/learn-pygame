81e985363ab73ee32e9537b9706d0cc161edfd4c
hyunsang4072 <hyunsang4072@gmail.com>
2023-05-31 23:37:19 -0700

    playing with animation in pygame

--- animations/animations.py
+++ animations/animations.py
@@ -3,10 +3,39 @@ import pygame, sys, random as r
 class Player(pygame.sprite.Sprite):  # Follow mouse cursor
     def __init__(self, pos_x, pos_y):
         super().__init__()
-        self.image = pygame.Surface([20, 20])
-        self.image.fill((255, 255, 255))
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load("./animations/frog_jump_complete_anim.gif"))
+        self.sprites.append(pygame.image.load("./animations/Frog_r_attack_anim.gif"))
+        self.sprites.append(pygame.image.load("./animations/frog_r_idle.gif"))
+        self.sprites.append(pygame.image.load("./animations/spr_frog_b_attack_anim.gif"))
+        self.sprites.append(pygame.image.load("./animations/spr_frog_b_idle_anim.gif"))
+        self.sprites.append(pygame.image.load("./animations/spr_frog_g_attack_anim.gif"))
+        self.sprites.append(pygame.image.load("./animations/spr_frog_g_idle_anim.gif"))
+        self.sprites.append(pygame.image.load("./animations/spr_frog_g_jump_1launch_anim.gif"))
+        self.sprites.append(pygame.image.load("./animations/spr_frog_g_jump_2up.gif"))
+        self.sprites.append(pygame.image.load("./animations/spr_frog_r_attack_anim.gif"))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
         self.rect = self.image.get_rect()
         self.rect.topleft = [pos_x, pos_y]
+
+    def animate(self):
+        self.is_animating = True
+    
+    def update(self):
+        if self.is_animating:
+            # self.current_sprite += 1
+            self.current_sprite += 0.3
+
+            # After one animation...
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            # self.image = self.sprites[self.current_sprite]
+            self.image = self.sprites[int(self.current_sprite)]
 
 # General setup
 pygame.init()
@@ -20,7 +49,7 @@ pygame.display.set_caption("Sprite Animation")
 
 # Create sprites and groups
 moving_sprites = pygame.sprite.Group()
-player = Player(100, 100)
+player = Player(10, 10)
 moving_sprites.add(player)
 
 while True:
@@ -28,13 +57,13 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+        if event.type == pygame.KEYDOWN:
+            player.animate()
     
-
-
-
 
     # Drawing
     screen.fill((0, 0, 0))
     moving_sprites.draw(screen)
+    moving_sprites.update()
     pygame.display.flip()
     clock.tick(60)
